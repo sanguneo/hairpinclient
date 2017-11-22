f4832a293fe16d6d0e179809d952338f920197c9
sanguneo <sknah0319@gmail.com>
2017-11-22 15:59:12 +0900

    - googleads

--- src/components/GoogleBottomAd.jsx
+++ src/components/GoogleBottomAd.jsx
@@ -9,17 +9,17 @@ class GoogleBottomAd extends React.Component {
 	constructor(props) {
 		super(props);
 	}
+	componentDidMount() {
+		(adsbygoogle = window.adsbygoogle || []).push({});
+	}
 	render() {
 		let classList = 'googlebottomad';
 		classList += this.props.app.bottomad ? ' showed' : '';
 		return (<div className={classList}>
-			<script type='text/javascript'>
-				google_ad_client = 'ca-pub-4210320191405594';
-				google_ad_slot = '7313477468';
-				google_ad_width = 320;
-				google_ad_height = 50;
-			</script>
-			<script type='text/javascript' src='//pagead2.googlesyndication.com/pagead/show_ads.js'></script>
+			<ins className="adsbygoogle"
+				 style={{display:'inline-block',width:320 + 'px',height:50 + 'px'}}
+				 data-ad-client="ca-pub-4210320191405594"
+				 data-ad-slot="7313477468"></ins>
 		</div>);
 	}
 }
